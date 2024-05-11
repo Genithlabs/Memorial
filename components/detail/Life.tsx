bbe26abefc7f5524036daa57434b7fbd06f0f7d2
june <lzao@naver.com>
2024-05-11 14:03:52 +0900

    방문객 메시지를 작성할 수 있습니다

--- components/detail/Life.tsx
+++ components/detail/Life.tsx
@@ -3,73 +3,129 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import TextAreaCustomized from "./TextAreaCustomized";
-import {useState} from "react";
+import { useState, ChangeEvent } from "react";
 import Grow from "@mui/material/Grow";
-import {LifeProps} from "./interfaces";
+import { LifeProps } from "./interfaces";
+import { useSession } from "next-auth/react";
 
-export default function Life({visitorMessages, detail, memorialId}:LifeProps) {
-
+export default function Life({ visitorMessages: initialVisitorMessages, detail, memorialId }: LifeProps) {
+	const [visitorMessages, setVisitorMessages] = useState(initialVisitorMessages);
+	const { data: session } = useSession();
 	const [showTextArea, setShowTextArea] = useState(false);
+	const [message, setMessage] = useState("");
 
-	const handleButtonClick = (flag:boolean) => {
+	const handleButtonClick = (flag: boolean) => {
 		setShowTextArea(flag);
+	};
+
+	const handleRegisterComment = async () => {
+		try {
+			if (session) {
+				if (!message) {
+					alert("메시지를 입력해주세요.")
+					return false;
+				}
+				const url = `${process.env.NEXT_PUBLIC_API_URL}/api/memorial/${memorialId}/comment/register`;
+				const formData = new FormData();
+				formData.append("message", message);
+				const response = await fetch(url, {
+					method: "POST",
+					headers: {
+						Authorization: `Bearer ${session.accessToken}`,
+					},
+					body: formData,
+				});
+				const result = await response.json();
+				if (result.result === "success") {
+					// 새로운 코멘트를 기존 메시지 리스트에 추가
+					setVisitorMessages([...result.data]);
+					setMessage("");
+				}
+			}
+		} catch (error) {
+			console.error("ERROR: ", error);
+		}
+	};
+
+	const handleMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+		setMessage(e.target.value);
 	};
 
 	return (
 		<>
-			<Container sx={{
-				p: "1rem",
-				fontSize: "1rem",
-			}}
-			  className={"diff-card-section"}
+			<Container
+				sx={{
+					p: "1rem",
+					fontSize: "1rem",
+				}}
+				className={"diff-card-section"}
 			>
 				{detail && <div dangerouslySetInnerHTML={{ __html: detail.career_contents }} />}
 			</Container>
 			<Box>
-				<Typography sx={{
-					fontSize: '2rem',
-					fontWeight: 'bold',
-					mt: "5rem",
-				}}>
+				<Typography
+					sx={{
+						fontSize: "2rem",
+						fontWeight: "bold",
+						mt: "5rem",
+					}}
+				>
 					방문객 메시지
 				</Typography>
 
-				{!showTextArea &&
-					<Grow in={!showTextArea}
-                      style={{ transformOrigin: '0 0 0' }}
-				      {...(!showTextArea ? { timeout: 200 } : {})}
+				{!showTextArea && (
+					<Grow
+						in={!showTextArea}
+						style={{ transformOrigin: "0 0 0" }}
+						{...(!showTextArea ? { timeout: 200 } : {})}
 					>
 						<Box>
-                            <Button variant="contained" onClick={() => handleButtonClick(true)}>메시지 쓰기</Button>
-                        </Box>
+							<Button variant="contained" onClick={() => handleButtonClick(true)}>
+								메시지 쓰기
+							</Button>
+						</Box>
 					</Grow>
-				}
+				)}
 
 				{showTextArea && (
-					<Grow in={showTextArea}
-				      style={{ transformOrigin: '0 0 0' }}
-				      {...(showTextArea ? { timeout: 200 } : {})}
+					<Grow
+						in={showTextArea}
+						style={{ transformOrigin: "0 0 0" }}
+						{...(showTextArea ? { timeout: 200 } : {})}
 					>
 						<Box>
-							<TextAreaCustomized placeholder="방문객 메시지를 여기에 작성하세요." />
-							<Box sx={{ textAlign: 'right' }}>
-								<Button variant="contained" color="error" sx={{ mr: '.5rem' }} onClick={() => handleButtonClick(false)}>취소</Button>
-								<Button variant="contained">게시하기</Button>
+							<TextAreaCustomized placeholder="방문객 메시지를 여기에 작성하세요." onChange={handleMessageChange} value={message} />
+							<Box sx={{ textAlign: "right" }}>
+								<Button
+									variant="contained"
+									color="error"
+									sx={{ mr: ".5rem" }}
+									onClick={() => handleButtonClick(false)}
+								>
+									취소
+								</Button>
+								<Button variant="contained" onClick={handleRegisterComment}>
+									게시하기
+								</Button>
 							</Box>
 						</Box>
 					</Grow>
 				)}
 			</Box>
 			<Box sx={{ mt: "1rem" }}>
-				{visitorMessages?.map(({created_at, message, user_name}, index) => {
+				{visitorMessages?.map(({ created_at, message, user_name }, index) => {
 					const date = new Date(created_at);
 					const formattedDate = `${date.getMonth() + 1}월 ${date.getDate()}일`;
 
 					return (
-						<Box key={index} sx={{ p: '1rem', mt: index !== 0 ? '2rem' : '0' }} className={"diff-card-section"}>
-							<div style={{display:"flex"}}>
+						<Box
+							key={index}
+							sx={{ p: "1rem", mt: index !== 0 ? "2rem" : "0" }}
+							className={"diff-card-section"}
+						>
+							<div style={{ display: "flex" }}>
 								<Typography>{user_name}</Typography>
-								<Typography sx={{ p: "0 .5rem"}}>•</Typography>
+								<Typography sx={{ p: "0 .5rem" }}>•</Typography>
 								<Typography>{formattedDate}</Typography>
 							</div>
 							<div>
