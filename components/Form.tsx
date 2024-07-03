9730b5a86d355eeb372d5eb67f2b7669adb616a3
june <lzao@naver.com>
2024-07-03 23:19:20 +0900

    스탭퍼 클릭 시 이동하고 edit, register api 호출 구간을 변경합니다

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import Basic from "../components/form/Basic";
 import Birth from "../components/form/Birth";
@@ -11,30 +11,29 @@ import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import {useSession} from "next-auth/react";
+import { useSession } from "next-auth/react";
 import dayjs from "dayjs";
-
 
 const steps = ['기본 정보 입력', '생애 입력', '건립한 기념관 보기'];
 
-function getStepContent(step: number, router:any, basicInfo: any, setBasicInfo: any, content: any, setContent: any, memorialId: any) {
+function getStepContent(step: number, router: any, basicInfo: any, setBasicInfo: any, content: any, setContent: any, memorialId: any) {
 	switch (step) {
 		case -1:
 			router.push('/');
-			return null; // or you can return an empty fragment <> </>
+			return null;
 		case 0:
 			return <Basic basicInfo={basicInfo} setBasicInfo={setBasicInfo} />;
 		case 1:
 			return <Birth content={content} setContent={setContent} />;
 		case 2:
-			return <Preview memorialId={memorialId}/>;
+			return <Preview memorialId={memorialId} basicInfo={basicInfo} content={content}/>;
 		default:
 			throw new Error('Unknown step');
 	}
 }
 
 export default function Form() {
-	const {data: session} = useSession();
+	const { data: session } = useSession();
 	const router = useRouter();
 	const [activeStep, setActiveStep] = useState(0);
 	const [basicInfo, setBasicInfo] = useState({
@@ -83,67 +82,25 @@ export default function Form() {
 		return value instanceof File;
 	};
 
-
-	const handleNext = async() => {
+	const handleNext = async () => {
 		if (activeStep === 0) {
-			if (!basicInfo.user_name || !basicInfo.birth_start || !basicInfo.profile ) {
+			if (!basicInfo.user_name || !basicInfo.birth_start || !basicInfo.profile) {
 				alert("모든 정보를 입력해 주세요.");
 				return;
 			}
+			if (memorialId) {
+				await handleSubmit();
+			}
 			setActiveStep(1);
-		}
-		if (activeStep === 1) {
+		} else if (activeStep === 1) {
 			if (!content) {
 				alert("모든 정보를 입력해 주세요.");
 				return;
 			}
-
-			// FormData 객체 생성
-			const formData = new FormData();
-			formData.append('user_name', basicInfo.user_name);
-			formData.append('birth_start', basicInfo.birth_start ? basicInfo.birth_start : dayjs().format('YYYY-MM-DD'));
-			if (basicInfo.birth_end) {
-				formData.append('birth_end', basicInfo.birth_end);
+			if (memorialId) {
+				await handleSubmit();
 			}
-			if (basicInfo.profile && isFile(basicInfo.profile)) {
-				formData.append('profile', basicInfo.profile);
-			}
-			if (basicInfo.bgm && isFile(basicInfo.bgm)) {
-				formData.append('bgm', basicInfo.bgm);
-			}
-			formData.append('career', content);
-
-			const url = `${memorialId ? 
-				`${process.env.NEXT_PUBLIC_API_URL}/api/memorial/${memorialId}/edit` : 
-				`${process.env.NEXT_PUBLIC_API_URL}/api/memorial/register`}`;
-
-			try {
-				if (session) {
-					const response = await fetch(url, {
-						method: 'POST',
-						headers: {
-							'Authorization': `Bearer ${session.accessToken}`
-						},
-						body: formData,
-					});
-					const result = await response.json();
-					if (!response.ok || result.result === 'fail') {
-						const errorMessage = result.message ?
-							(Array.isArray(result.message) ? result.message.join(', ') : result.message)
-							: 'ERROR!!';
-						alert(`ERROR: ${errorMessage}`);
-					} else {
-						if (result.data) {
-							setMemorialId(result.data.id);
-						}
-						setActiveStep(2);
-					}
-				} else {
-					console.error('Session is null');
-				}
-			} catch (error) {
-				console.log(error);
-			}
+			setActiveStep(2);
 		}
 	};
 
@@ -151,8 +108,60 @@ export default function Form() {
 		setActiveStep(activeStep - 1);
 	};
 
-	const handleStepClick = (step: number) => {
-		setActiveStep(step);
+	const handleStepClick = async (step: number) => {
+		if (step < activeStep) {
+			setActiveStep(step);
+		} else {
+			if (memorialId) {
+				await handleSubmit();
+			}
+			setActiveStep(step);
+		}
+	};
+
+	const handleSubmit = async () => {
+		const formData = new FormData();
+		formData.append('user_name', basicInfo.user_name);
+		formData.append('birth_start', basicInfo.birth_start ? basicInfo.birth_start : dayjs().format('YYYY-MM-DD'));
+		if (basicInfo.birth_end) {
+			formData.append('birth_end', basicInfo.birth_end);
+		}
+		if (basicInfo.profile && isFile(basicInfo.profile)) {
+			formData.append('profile', basicInfo.profile);
+		}
+		if (basicInfo.bgm && isFile(basicInfo.bgm)) {
+			formData.append('bgm', basicInfo.bgm);
+		}
+		formData.append('career', content);
+
+		const url = `${process.env.NEXT_PUBLIC_API_URL}/api/memorial/${memorialId}/edit`
+
+		try {
+			if (session) {
+				const response = await fetch(url, {
+					method: 'POST',
+					headers: {
+						'Authorization': `Bearer ${session.accessToken}`
+					},
+					body: formData,
+				});
+				const result = await response.json();
+				if (!response.ok || result.result === 'fail') {
+					const errorMessage = result.message ?
+						(Array.isArray(result.message) ? result.message.join(', ') : result.message)
+						: 'ERROR!!';
+					alert(`ERROR: ${errorMessage}`);
+				} else {
+					if (result.data) {
+						setMemorialId(result.data.id);
+					}
+				}
+			} else {
+				console.error('Session is null');
+			}
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	return (
@@ -162,7 +171,7 @@ export default function Form() {
 					<Typography component="h1" variant="h4" align="center">
 						인생 기념관 만들기
 					</Typography>
-					<Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5,  }}>
+					<Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
 						{steps.map((label, index) => (
 							<Step key={label} onClick={() => handleStepClick(index)} completed={false}>
 								<StepLabel>{label}</StepLabel>
@@ -175,14 +184,14 @@ export default function Form() {
 							<Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
 								뒤로
 							</Button>
-							{activeStep < (steps.length-1) &&
-							<Button
-								onClick={handleNext}
-								sx={{ mt: 3, ml: 1 }}
-								color="inherit"
-							>
-								다음
-							</Button>
+							{activeStep < (steps.length - 1) &&
+								<Button
+									onClick={handleNext}
+									sx={{ mt: 3, ml: 1 }}
+									color="inherit"
+								>
+									다음
+								</Button>
 							}
 						</Box>
 					</>
--- components/form/Preview.tsx
+++ components/form/Preview.tsx
@@ -2,128 +2,172 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from "@mui/material/Container";
-import NextLink from "next/link";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Paper from "@mui/material/Paper";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-export default function Preview({memorialId: memorialId}: any) {
+export default function Preview({ memorialId, basicInfo, content }: any) {
+	const { data: session } = useSession();
+	const router = useRouter();
+
+	const handleFinalize = async () => {
+		try {
+			if (!memorialId) {
+				if (session) {
+					const formData = new FormData();
+					formData.append('user_name', basicInfo.user_name);
+					formData.append('birth_start', basicInfo.birth_start);
+					if (basicInfo.birth_end) {
+						formData.append('birth_end', basicInfo.birth_end);
+					}
+					if (basicInfo.profile) {
+						formData.append('profile', basicInfo.profile);
+					}
+					if (basicInfo.bgm) {
+						formData.append('bgm', basicInfo.bgm);
+					}
+					formData.append('career', content);
+
+					const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/memorial/register`, {
+						method: 'POST',
+						headers: {
+							'Authorization': `Bearer ${session.accessToken}`,
+						},
+						body: formData,
+					});
+
+					const result = await response.json();
+					if (response.ok && result.result === 'success') {
+						alert('기념관이 성공적으로 등록되었습니다.');
+						router.push(`/detail/${result.data.id}`);
+					} else {
+						const errorMessage = result.message ?
+							(Array.isArray(result.message) ? result.message.join(', ') : result.message)
+							: 'ERROR!!';
+						alert(`ERROR: ${errorMessage}`);
+					}
+				}
+			} else {
+				router.push(`/detail/${memorialId}`);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
-		<>
-			<Container component="main"
-			           sx={{
-						   display: "flex",
-				           flexDirection: "column",
-				           justifyContent: "center",
-				           alignItems: "center",
-			}}>
-
+		<Container component="main"
+				   sx={{
+					   display: "flex",
+					   flexDirection: "column",
+					   justifyContent: "center",
+					   alignItems: "center",
+				   }}>
+			<Typography
+				align="center"
+				color="text.primary"
+				gutterBottom
+				sx={{
+					fontSize: {
+						xs: '1.3rem',
+						sm: '1.3rem',
+						md: '1.3rem'
+					},
+					fontWeight: "bold",
+					wordBreak: 'keep-all',
+					mt: 6
+				}}
+			>
+				앞으로 30일 동안 기념관을 무료로 이용할 수 있습니다.
+			</Typography>
+			<Button variant="contained" sx={{ mt: 2, fontSize: "1rem", p: 2, fontWeight: "bold" }} onClick={handleFinalize}>건립한 기념관 보기</Button>
+			<Typography
+				align="center"
+				color="text.primary"
+				gutterBottom
+				sx={{
+					fontSize: {
+						xs: '1.3rem',
+						sm: '1.3rem',
+						md: '1.3rem'
+					},
+					fontWeight: "bold",
+					mt: 10,
+					wordBreak: 'keep-all',
+				}}
+			>
+				단 한 번의 지출로 영원한 당신의 기념관을 건립하세요.
+			</Typography>
+			<div>
 				<Typography
-					align="center"
+					align="left"
 					color="text.primary"
 					gutterBottom
 					sx={{
 						fontSize: {
-							xs: '1.3rem',  // extra-small devices에서의 폰트 크기
-							sm: '1.3rem', // small devices에서의 폰트 크기
-							md: '1.3rem'  // medium devices에서의 폰트 크기
+							xs: '1rem',
+							sm: '1rem',
+							md: '1.3rem'
 						},
-						fontWeight: "bold",
-						wordBreak: 'keep-all',
-						mt: 6
-					}}
-				>
-					앞으로 30일 동안 기념관을 무료로 이용할 수 있습니다.
-				</Typography>
-				<NextLink href={`/detail/${memorialId}`}>
-					<Button variant="contained" sx={{ mt: 2, fontSize: "1rem", p: 2, fontWeight: "bold" }}>건립한 기념관 보기</Button>
-				</NextLink>
-				<Typography
-					align="center"
-					color="text.primary"
-					gutterBottom
-					sx={{
-						fontSize: {
-							xs: '1.3rem',  // extra-small devices에서의 폰트 크기
-							sm: '1.3rem', // small devices에서의 폰트 크기
-							md: '1.3rem'  // medium devices에서의 폰트 크기
-						},
-						fontWeight: "bold",
-						mt: 10,
+						mt: 4,
 						wordBreak: 'keep-all',
 					}}
 				>
-					단 한 번의 지출로 영원한 당신의 기념관을 건립하세요.
+					당신의 기념관을 건립하면?
 				</Typography>
-				<div>
-					<Typography
-						align="left"
-						color="text.primary"
-						gutterBottom
-						sx={{
-							fontSize: {
-								xs: '1rem',  // extra-small devices에서의 폰트 크기
-								sm: '1rem', // small devices에서의 폰트 크기
-								md: '1.3rem'  // medium devices에서의 폰트 크기
-							},
-							mt: 4,
-							wordBreak: 'keep-all',
-						}}
-					>
-						당신의 기념관을 건립하면?
-					</Typography>
-					<List sx={{ listStyleType: 'upper-roman', pl: { xs: 4, md: 4 }}}>
-						<ListItem sx={{ display: 'list-item' }}>
-							<ListItemText primary="한 사람의 추억과 인생을 특별하게 남길 수 있습니다" />
-						</ListItem>
-						<ListItem sx={{ display: 'list-item' }}>
-							<ListItemText primary="인생을 돌아보며 정리하고 기념할 수 있습니다" />
-						</ListItem>
-						<ListItem sx={{ display: 'list-item' }}>
-							<ListItemText primary="그 사람이 그리워질 때 다시 찾을 장소를 만들 수 있습니다" />
-						</ListItem>
-						<ListItem sx={{ display: 'list-item' }}>
-							<ListItemText primary="다른 가족들, 친구들, 지인들에게 메시지를 남길 수 있습니다" />
-						</ListItem>
-					</List>
-				</div>
-				<Paper elevation={4} sx={{ mt: 5, p: { xs: 4, md: 5 }, display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}>
-					<Typography sx={{
-						fontSize: {
-							xs: '1.3rem',  // extra-small devices에서의 폰트 크기
-							sm: '1.3rem', // small devices에서의 폰트 크기
-							md: '1.3rem'  // medium devices에서의 폰트 크기
-						},
-						fontWeight: "bold"
-						}}>
-						영원한 기념관 건립
-					</Typography>
-					<Typography sx={{
-						fontSize: {
-							xs: '1.1rem',  // extra-small devices에서의 폰트 크기
-							sm: '1.1rem', // small devices에서의 폰트 크기
-							md: '1.1rem'  // medium devices에서의 폰트 크기
-						},
-						fontWeight: "bold"
-					}}>
-						50,000원 <del>100,000원</del>
-					</Typography>
-					<List sx={{ listStyleType: 'disc'}}>
-						<ListItem sx={{ display: 'list-item', mb: -2 }}>
-							<ListItemText primary={<Typography sx={{ marginLeft: "-1rem", fontSize: {xs:'.9rem', sm:'1rem', md:'1rem'}}}>입금처 : 기업 11118628601014 민준우</Typography>} />
-						</ListItem>
-						<ListItem sx={{ display: 'list-item', mb: -2 }}>
-							<ListItemText primary={<Typography sx={{ marginLeft: "-1rem", fontSize: {xs:'.9rem', sm:'1rem', md:'1rem'} }}>입금하신 후 : </Typography>} />
-							<List sx={{ listStyleType: 'circle', margin: 0, padding: 0}}>
-								<ListItem sx={{ display: 'list-item', mt: -2 }}>
-									<ListItemText primary={<Typography sx={{ marginLeft: "-1rem", marginRight: "-3rem", fontSize: {xs:'.9rem', sm:'1rem', md:'1rem'} }}>010-9658-5329 으로 기념인 이름(성함), 입금하신 계좌주 명을 보내주세요</Typography>} />
-								</ListItem>
-							</List>
-						</ListItem>
-					</List>
-				</Paper>
-			</Container>
-		</>
+				<List sx={{ listStyleType: 'upper-roman', pl: { xs: 4, md: 4 } }}>
+					<ListItem sx={{ display: 'list-item' }}>
+						<ListItemText primary="한 사람의 추억과 인생을 특별하게 남길 수 있습니다" />
+					</ListItem>
+					<ListItem sx={{ display: 'list-item' }}>
+						<ListItemText primary="인생을 돌아보며 정리하고 기념할 수 있습니다" />
+					</ListItem>
+					<ListItem sx={{ display: 'list-item' }}>
+						<ListItemText primary="그 사람이 그리워질 때 다시 찾을 장소를 만들 수 있습니다" />
+					</ListItem>
+					<ListItem sx={{ display: 'list-item' }}>
+						<ListItemText primary="다른 가족들, 친구들, 지인들에게 메시지를 남길 수 있습니다" />
+					</ListItem>
+				</List>
+			</div>
+			<Paper elevation={4} sx={{ mt: 5, p: { xs: 4, md: 5 }, display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}>
+				<Typography sx={{
+					fontSize: {
+						xs: '1.3rem',
+						sm: '1.3rem',
+						md: '1.3rem'
+					},
+					fontWeight: "bold"
+				}}>
+					영원한 기념관 건립
+				</Typography>
+				<Typography sx={{
+					fontSize: {
+						xs: '1.1rem',
+						sm: '1.1rem',
+						md: '1.1rem'
+					},
+					fontWeight: "bold"
+				}}>
+					50,000원 <del>100,000원</del>
+				</Typography>
+				<List sx={{ listStyleType: 'disc' }}>
+					<ListItem sx={{ display: 'list-item', mb: -2 }}>
+						<ListItemText primary={<Typography sx={{ marginLeft: "-1rem", fontSize: { xs: '.9rem', sm: '1rem', md: '1rem' } }}>입금처 : 기업 11118628601014 민준우</Typography>} />
+					</ListItem>
+					<ListItem sx={{ display: 'list-item', mb: -2 }}>
+						<ListItemText primary={<Typography sx={{ marginLeft: "-1rem", fontSize: { xs: '.9rem', sm: '1rem', md: '1rem' } }}>입금하신 후 : </Typography>} />
+						<List sx={{ listStyleType: 'circle', margin: 0, padding: 0 }}>
+							<ListItem sx={{ display: 'list-item', mt: -2 }}>
+								<ListItemText primary={<Typography sx={{ marginLeft: "-1rem", marginRight: "-3rem", fontSize: { xs: '.9rem', sm: '1rem', md: '1rem' } }}>010-9658-5329 으로 기념인 이름(성함), 입금하신 계좌주 명을 보내주세요</Typography>} />
+							</ListItem>
+						</List>
+					</ListItem>
+				</List>
+			</Paper>
+		</Container>
 	);
 }
