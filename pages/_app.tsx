35cb0e06fb8ce80355a496356422103b0d5f5a6e
june <lzao@naver.com>
2024-07-08 23:29:27 +0900

    GA4 를 제거합니다

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,35 +7,14 @@ import {makeTheme} from "@/utils/theme";
 import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
 import {SessionProvider} from "next-auth/react";
-import {useRouter} from 'next/router';
-import {analytics} from '@/firebase';
-import {logEvent} from 'firebase/analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [mode, setMode] = useState<'light' | 'dark'>('light');
-  const router = useRouter();
 
   useEffect(() => {
     const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
     setMode(prefersDarkMode ? 'dark' : 'light');
   }, []);
-
-    useEffect(() => {
-        const handleRouteChange = (url: string) => {
-            if (analytics) {
-                logEvent(analytics, 'page_view', {
-                    page_path: url,
-                });
-            }
-        };
-
-        router.events.on("routeChangeComplete", handleRouteChange);
-        handleRouteChange(window.location.pathname);
-
-        return () => {
-            router.events.off("routeChangeComplete", handleRouteChange);
-        };
-    }, [router.events]);
 
   const theme = makeTheme(mode);
 
