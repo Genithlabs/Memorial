76f20a84d2f989e73d2c93ca26a3256b8240ad50
june <lzao@naver.com>
2024-07-28 14:48:09 +0900

    useEffect 사용 시 ssr 환경에서 window 객체를 사용하는 경우 발생하는 에러를 수정합니다

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,42 +1,44 @@
 import { useState, useEffect } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import {makeTheme} from "@/utils/theme";
-import Footer from "../components/common/Footer";
-import Header from "../components/common/Header";
-import {SessionProvider} from "next-auth/react";
-import {useRouter} from 'next/router';
-import {analytics} from '@/firebase';
-import {logEvent} from 'firebase/analytics';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { makeTheme } from '@/utils/theme';
+import Footer from '../components/common/Footer';
+import Header from '../components/common/Header';
+import { SessionProvider } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { analytics } from '@/firebase';
+import { logEvent } from 'firebase/analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [mode, setMode] = useState<'light' | 'dark'>('light');
   const router = useRouter();
 
   useEffect(() => {
-    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    setMode(prefersDarkMode ? 'dark' : 'light');
+    if (typeof window !== 'undefined') {
+      const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      setMode(prefersDarkMode ? 'dark' : 'light');
+    }
   }, []);
 
-    useEffect(() => {
-      if (process.env.APP_ENV_VALUE !== 'local') {
-        const handleRouteChange = (url: string) => {
-          if (analytics) {
-            logEvent(analytics, 'page_view', {
-              page_path: url,
-            });
-          }
-        };
-
-        router.events.on("routeChangeComplete", handleRouteChange);
-          handleRouteChange(window.location.pathname);
-            return () => {
-              router.events.off("routeChangeComplete", handleRouteChange);
-            };
+  useEffect(() => {
+    if (typeof window !== 'undefined' && process.env.APP_ENV_VALUE !== 'local') {
+      const handleRouteChange = (url: string) => {
+        if (analytics) {
+          logEvent(analytics, 'page_view', {
+            page_path: url,
+          });
         }
-    }, [router.events]);
+      };
+
+      router.events.on('routeChangeComplete', handleRouteChange);
+      handleRouteChange(window.location.pathname);
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange);
+      };
+    }
+  }, [router.events]);
 
   const theme = makeTheme(mode);
 
@@ -44,9 +46,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <SessionProvider session={pageProps.session}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Header/>
+          <Header />
           <Component {...pageProps} />
-          <Footer/>
+          <Footer />
         </ThemeProvider>
       </SessionProvider>
   );
